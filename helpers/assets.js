d60058350ae85ff719748a279ae800ba023f100b
Bodegas <bodegas.mail@gmail.com>
2020-02-25 15:30:43 +0100

    Create new project robot

---
+++ helpers/assets.js
@@ -0,0 +1,25 @@
+// DEEP EQUAL
+export const objectA = {
+  k1: 1,
+  k2: 2,
+  k3: {
+    k31: 31,
+    k32: 32,
+    k33: { k33: "fddf" }
+  },
+  k4: 4
+};
+
+export const objectB = {
+  k1: 1,
+  k2: 2,
+  k3: {
+    k31: 31,
+    k32: 32,
+    k33: { k33: "fddf" }
+  },
+  k4: 4
+};
+
+// FLATTEN
+export const arrayA = [2, 3, [21, 22], 4, [88, 43, 7, [0, 1, [7, 95]]], 9];
--- helpers/index.js
+++ helpers/index.js
@@ -1,33 +1,9 @@
 import { deepEqual } from "./DeepEqual";
 import { flattenArray } from "./Flatten";
-
-// DEEP EQUAL
-const a = {
-  k1: 1,
-  k2: 2,
-  k3: {
-    k31: 31,
-    k32: 32,
-    k33: { k33: "fddf" }
-  },
-  k4: 4
-};
-
-const b = {
-  k1: 1,
-  k2: 2,
-  k3: {
-    k31: 31,
-    k32: 32,
-    k33: { k33: "fddf" }
-  },
-  k4: 4
-};
+import { objectA, objectB, arrayA } from "./assets.js";
 
 console.log("DEEP EQUAL");
-console.log(deepEqual(a, b));
+console.log(deepEqual(objectA, objectB));
 
-// FLATTEN
-const array1 = [2, 3, [21, 22], 4, [88, 43, 7, [0, 1, [7, 95]]], 9];
 console.log("FLATTEN");
-console.log(flattenArray(array1));
+console.log(flattenArray(arrayA));
